0de92a3924061b716fc67d7627201721e1bc5f1c
jimmy-lew <jimmylew9779@gmail.com>
2026-02-16 15:52:59 +0800

    feat: setup app seo

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,43 @@
+<script setup lang="ts">
+useHead({
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+  ],
+  link: [{ rel: 'icon', href: '/favicon.png' }],
+  htmlAttrs: {
+    lang: 'en',
+  },
+  bodyAttrs: { },
+})
+
+const title = 'Ledgr'
+const description = ''
+
+useSeoMeta({
+  title,
+  description,
+  ogTitle: title,
+  ogDescription: description,
+  ogUrl: '',
+  ogImage: '',
+  twitterTitle: title,
+  twitterDescription: description,
+  twitterImage: '',
+  twitterCard: 'summary_large_image',
+})
+
+const router = useRouter()
+
+defineShortcuts({
+  'G-H': () => router.push('/'),
+})
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <UApp>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </UApp>
 </template>
