ddf75023771b1bbc7d1cda3cc5535316c5c296d3
jimmy-lew <jimmylew9779@gmail.com>
2026-02-16 15:23:21 +0800

    feat: add tailwind and vueuse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxt/eslint', '@vueuse/nuxt'],
+  ssr: false,
+  css: ['~/assets/css/tailwind.css'],
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true }
 })
