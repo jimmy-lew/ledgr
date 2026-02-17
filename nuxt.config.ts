29439f788c242fe93e9031445663ef1041b21798
jimmy-lew <jimmylew9779@gmail.com>
2026-02-17 13:07:39 +0800

    deps: add motion

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@vueuse/nuxt', '@nuxt/hints', '@nuxt/fonts', '@nuxt/ui'],
+  modules: ['@nuxt/eslint', '@vueuse/nuxt', '@nuxt/hints', '@nuxt/fonts', '@nuxt/ui', 'motion-v/nuxt'],
   ssr: false,
   css: ['~/assets/css/tailwind.css'],
   compatibilityDate: '2025-07-15',
