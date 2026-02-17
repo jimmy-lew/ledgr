2c44ef0c49d8bc4091d342dfc76a41c73eea95d4
jimmy-lew <jimmylew9779@gmail.com>
2026-02-17 13:09:08 +0800

    temp: temp defaults for pages

---
+++ app/layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+<!-- <UMain class="bg-black text-white"> -->
+  <slot />
+<!-- </UMain> -->
+</template>
+
+<style scoped>
+
+</style>
---
+++ app/pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+<h1 class="font-header text-5xl font-black">
+  Test
+</h1>
+<ActionBar />
+</template>
+
+<style scoped>
+</style>
