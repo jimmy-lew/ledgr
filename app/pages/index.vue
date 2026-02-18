003e6a9440aae0644b48cdc6721d0bc2c918714d
jimmy-lew <jimmylew9779@gmail.com>
2026-02-18 21:27:55 +0800

    feat: update index page gradient

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,11 +2,13 @@
 </script>
 
 <template>
-<div class="relative flex flex-col items-center justify-end w-full h-[40vh] rounded-b-4xl bg-linear-to-br from-emerald-300 to-yellow-200">
+<div class="relative flex flex-col items-center justify-end w-full h-[40vh] rounded-b-4xl bg-black overflow-hidden">
+  <div class="absolute -top-20 -left-15 w-80 h-80 bg-blue-600/40 rounded-full blur-[90px]"></div>
+  <div class="absolute -top-10 left-5 w-72 h-64 bg-emerald-500 rounded-full blur-[100px]"></div>
   <div class="absolute rounded-3xl bg-cover bg-no-repeat bg-[url('https://i.pinimg.com/736x/d6/ac/44/d6ac44091afa772b3744f1ecad3d33e3.jpg')] w-72 h-42 right-12 bottom-24"></div>
-  <div class="absolute flex flex-col gap-4 p-8 rounded-3xl bg-white/10 backdrop-blur-md shadow-md w-72 h-42 left-12 bottom-18">
+  <div class="absolute flex flex-col gap-4 p-8 rounded-3xl bg-white/10 backdrop-blur-sm shadow-md w-72 h-42 left-12 bottom-18">
     <h2 class="text-white font-bold">Balance</h2>
-    <h1 class="text-3xl font-header font-black text-white">$22,000.00</h1>
+    <h1 class="text-3xl font-header font-black text-white">22,000.00</h1>
   </div>
 </div>
 </template>
