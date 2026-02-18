dec1c28d6015cf6cafe9e0269d2f5107b00cd03f
jimmy-lew <jimmylew9779@gmail.com>
2026-02-18 21:27:31 +0800

    feat: swap default to use HomeBar

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,43 +1,26 @@
 <script setup lang="ts">
-const _items = [{
-  title: 'Home',
-  icon: 'lucide:home'
-}, {
-  title: 'Transactions',
-  icon: 'lucide:file-clock',
-  items: [{
-    title: 'Expenses',
-    icon: 'lucide:chart-line',
-    subtrailing: 'WIP'
-  }, {
-    title: 'Goals',
-    icon: 'lucide:flag',
-    subtrailing: 'WIP'
-  }, {
-    title: 'Subscriptions',
-    icon: 'lucide:refresh-cw',
-    subtrailing: 'WIP'
-  }]
-}, {
-  title: 'Settings',
-  icon: 'lucide:bolt',
-  items: [{
-    title: 'Profile',
-    icon: 'lucide:user-round',
-    subtrailing: 'WIP'
-  }]
-}]
+const items = [
+  { icon: 'lucide:home' },
+  { icon: 'lucide:chart-line' },
+  {
+    icon: 'lucide:plus',
+    items: [
+      { icon: 'lucide:scan-line', title: 'Add statement' },
+      { icon: 'lucide:flag', title: 'Add goal' },
+      { icon: 'lucide:refresh-cw', title: 'Add subscription' },
+    ],
+  },
+  { icon: 'lucide:square' },
+  { icon: 'lucide:user-round' }
+]
 </script>
 
 <template>
 <div class="relative">
   <div class="min-h-screen flex flex-col bg-[#edf0ea] dark:bg-default">
     <slot />
-    <!-- <main class="flex-1 grid grid-cols-3 max-lg:grid-cols-2 max-md:grid-cols-1 gap-5 p-7 max-md:p-4 max-w-345 w-full mx-auto items-start" >
-    </main> -->
   </div>
-   <ActionBar :items="_items" />
-  <!-- <HomeBar /> -->
+   <HomeBar :items/>
 </div>
 </template>
 
