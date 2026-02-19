b8548f23f5ad13fe2807a7f3ecf9a56b52022ff9
jimmy-lew <jimmylew9779@gmail.com>
2026-02-19 09:28:41 +0800

    feat: migrate home insights to sfc

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -17,38 +17,7 @@
       </span>
     </div>
   </div>
-  <div class="relative px-6 w-full mt-64">
-    <div class="flex items-center justify-between rounded-2xl bg-default p-2">
-      <div class="flex flex-col gap-1 p-3 w-1/2">
-        <span class="text-xs text-muted">
-          Last month
-        </span>
-        <div class="flex items-start justify-start gap-1">
-          <span class="text-xl font-bold">
-            - 1,000
-          </span>
-          <span class="text-xs text-green-400">
-            <UIcon name="lucide:chevron-down"/>
-            1.5%
-          </span>
-        </div>
-      </div>
-      <div class="flex flex-col gap-1 p-3 w-1/2">
-        <span class="text-xs text-muted">
-          Weekly
-        </span>
-        <div class="flex items-start justify-start gap-1">
-          <span class="text-xl font-bold">
-            - 52
-          </span>
-          <span class="text-xs text-green-400">
-            <UIcon name="lucide:chevron-down"/>
-            150.2%
-          </span>
-        </div>
-      </div>
-    </div>
-  </div>
+  <HomeGlanceInsights />
   <HomeWidgets />
   <div class="px-6 w-full flex flex-col mt-3 gap-2">
     <h3 class="text-muted">Recent transactions</h3>
