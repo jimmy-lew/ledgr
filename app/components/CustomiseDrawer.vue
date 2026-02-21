65ea6f09ff1a74f4c66dfc047efec17d512fec47
jimmy-lew <jimmylew9779@gmail.com>
2026-02-21 15:59:21 +0800

    feat: baseline drawer impl

---
+++ app/components/CustomiseDrawer.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <UDrawer>
+    <UButton label="Open" color="neutral" variant="subtle" trailing-icon="i-lucide-chevron-up" />
+
+    <template #content>
+      <UEmpty class="h-48 m-4" />
+    </template>
+  </UDrawer>
+</template>
