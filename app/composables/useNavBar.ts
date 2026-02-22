bb5110415e6e3638d5eb6f281261072c6aeeda6b
jimmy-lew <jimmylew9779@gmail.com>
2026-02-22 19:53:00 +0800

    refactor: clean container state logic, moving bulk of calculation to a computed ref

--- app/composables/useNavBar.ts
+++ app/composables/useNavBar.ts
@@ -9,6 +9,12 @@ interface ContainerState {
   borderRadius: number
 }
 
+function objectMap<T extends object, K extends keyof T>(obj: T, f: (k: K, v: T[K]) => [K, T[K]]): T {
+  const entries = Object.entries(obj) as [K, T[K]][]
+  const mappedObject = entries.map(([k, v]) => f(k, v))
+  return Object.fromEntries(mappedObject) as T
+}
+
 export const useNavBar = createSharedComposable(() => {
   const router = useRouter()
   const itemRefs = ref<HTMLElement[]>([])
@@ -22,47 +28,30 @@ export const useNavBar = createSharedComposable(() => {
   const hoveredItem = ref<number>(-1)
   const activeItem = computed(() => hoveredItem.value >= 0 ? hoveredItem.value : selectedItem.value )
 
-  const containerState = ref<ContainerState>({ width: width.value, height: DEFAULT_HEIGHT, borderRadius: BORDER_RADIUS })
-  // Kinda ugly, will refactor later
-  const subContainerState = computed(() => Object.fromEntries(Object.entries(containerState.value).map(([k, v]) => [k, v - 2])))
-  const baseContainerState = toRaw(containerState.value)
-  const baseSubcontainerState = toRaw(subContainerState.value)
+  const containerState = computed<ContainerState>(() => {
+    const el = subItemRefs.value[hoveredItem.value]
+    if (!el) return { width: width.value, height: DEFAULT_HEIGHT, borderRadius: BORDER_RADIUS }
+    const rect = el.getBoundingClientRect()
+    return { width: Math.max(rect.width, width.value + 64), height: rect.height + 48, borderRadius: 18 }
+  })
+  const subContainerState = computed(() => objectMap(containerState.value, (k,v) => [k, v-2]))
 
   const select = (index: number, to?: string) => { selectedItem.value = index; if (to) router.push(to)}
   const subItemSelect = (index: number, si: number, cb?: () => void) => {
     select(index)
     selectedSubItem.value =  si
     hoveredItem.value = -1
-    containerState.value = baseContainerState
     cb?.()
   }
-  const handleHover = (index: number) => {
-    hoveredItem.value = index
-    const el = subItemRefs.value[index]
-    if (!el) {
-      containerState.value = baseContainerState
-      return
-    }
-    const rect = el.getBoundingClientRect()
-    containerState.value = {
-      width: Math.max(rect.width, width.value + 64),
-      height: rect.height + 48,
-      borderRadius: 18
-    }
-  }
+  const handleHover = (index: number) => { hoveredItem.value = index }
   const _handleHoverEnd = () => {
     const isHoveringSubItem = subItemRefs.value.some(el => el.matches(':hover'))
     const isHoveringItem = itemRefs.value.some(el => el.matches(':hover'))
-    if (!isHoveringItem && !isHoveringSubItem) {
-      hoveredItem.value = -1
-      containerState.value = baseContainerState
-    }
+    if (!isHoveringItem && !isHoveringSubItem) { hoveredItem.value = -1 }
   }
   const handleHoverEnd = () => { setTimeout(_handleHoverEnd, HOVER_DELAY) }
   const setItemRef = (index: number) => (el: any) => { if (el) itemRefs.value[index] = el }
   const setSubItemRef = (index: number) => (el: any) => { if (el) subItemRefs.value[index] = el.$el || el }
-
-  // watch([selectedItem, hoveredItem, activeItem], ([s, h, a]) => console.log(s,h,a))
 
   return {
     setItemRef, setSubItemRef,
