666f3fab89b1a22e840717f0a3f2e7ed4ea17634
jimmy-lew <jimmylew9779@gmail.com>
2026-02-22 22:38:11 +0800

    feat: add system information to settings page and export button (WIP)

--- app/pages/settings.vue
+++ app/pages/settings.vue
@@ -11,21 +11,21 @@ const isDark = computed({
 <div class="flex flex-col gap-2 p-6">
   <h1 class="text-xl font-bold">Settings</h1>
   <div class="flex flex-col gap-px mt-2">
-    <div class="flex items-center justify-between bg-black/5 dark:bg-white/5 hover:bg-black/10 dark:hover:bg-white/10 active:scale-95 transition-all duration-300 p-3 first:rounded-t-2xl last:rounded-b-2xl">
+    <div class="flex items-center justify-between bg-black/5 dark:bg-white/5 hover:bg-black/10 dark:hover:bg-white/10 active:scale-95 transition-all duration-300 p-3 first:rounded-t-3xl last:rounded-b-3xl">
       Workspace
       <span class="flex items-center gap-2">
         WIP
         <UIcon name="lucide:chevrons-up-down" />
       </span>
     </div>
-    <div class="flex items-center justify-between bg-black/5 dark:bg-white/5 hover:bg-black/10 dark:hover:bg-white/10 active:scale-95 transition-all duration-300 p-3 first:rounded-t-2xl last:rounded-b-2xl">
+    <div class="flex items-center justify-between bg-black/5 dark:bg-white/5 hover:bg-black/10 dark:hover:bg-white/10 active:scale-95 transition-all duration-300 p-3 first:rounded-t-3xl last:rounded-b-3xl">
       Notifications
       <span class="flex items-center gap-2">
         Disabled
         <UIcon name="lucide:move-up-right" />
       </span>
     </div>
-    <button @click="isDark = !isDark" class="flex items-center justify-between bg-black/5 dark:bg-white/5 hover:bg-black/10 dark:hover:bg-white/10 active:scale-95 transition-all duration-300 p-3 first:rounded-t-2xl last:rounded-b-2xl">
+    <button @click="isDark = !isDark" class="flex items-center justify-between bg-black/5 dark:bg-white/5 hover:bg-black/10 dark:hover:bg-white/10 active:scale-95 transition-all duration-300 p-3 first:rounded-t-3xl last:rounded-b-3xl">
       Appearance
       <span class="flex items-center gap-2 capitalize">
         {{ colorMode.preference }}
@@ -33,5 +33,16 @@ const isDark = computed({
       </span>
     </button>
   </div>
+  <div class="flex flex-col gap-px mt-2">
+    <div class="flex items-center justify-between bg-black/5 dark:bg-white/5 hover:bg-black/10 dark:hover:bg-white/10 active:scale-95 transition-all duration-300 p-3 first:rounded-t-3xl last:rounded-b-3xl">
+      Export data
+      <span class="flex items-center gap-2">
+        <UIcon name="lucide:move-up-right" />
+      </span>
+    </div>
+  </div>
+  <span class="w-full text-center text-xs text-muted mt-8">
+    Ledgr for Android 1.0.0 (220226-external)
+  </span>
 </div>
 </template>
