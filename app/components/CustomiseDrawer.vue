ec6ccac0759cf7916f545d4bb93c0f88236ca16d
jimmy-lew <jimmylew9779@gmail.com>
2026-03-14 21:47:42 +0800

    refactor: remove button

--- app/components/CustomiseDrawer.vue
+++ app/components/CustomiseDrawer.vue
@@ -4,8 +4,6 @@
 
 <template>
   <UDrawer>
-    <UButton label="Open" color="neutral" variant="subtle" trailing-icon="i-lucide-chevron-up" />
-
     <template #content>
       <UEmpty class="h-48 m-4" />
     </template>
