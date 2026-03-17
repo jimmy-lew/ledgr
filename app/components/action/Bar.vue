8e02c8efa90ca2c1689a1bae041050880853a55e
jimmy <jimmylew9779@gmail.com>
2026-03-17 11:20:02 +0800

    feat: tighten padding and spacing

--- app/components/action/Bar.vue
+++ app/components/action/Bar.vue
@@ -5,7 +5,7 @@ const props = defineProps<{ items: NavItem[] }>()
 
 const { activeItemRef, containerState, activeGroup } = useActionBar()
 
-const indicatorPos = computed(() => Math.max(activeItemRef.value?.offsetLeft ?? 22, 22))
+const indicatorPos = computed(() => Math.max(activeItemRef.value?.offsetLeft ?? 36, 36))
 
 // const handleKeydown = (e: KeyboardEvent) => {
 //   const current = selectedItem.value
@@ -35,8 +35,14 @@ const indicatorPos = computed(() => Math.max(activeItemRef.value?.offsetLeft ?? 
   />
 </div>
  -->
-<div class="fixed bottom-4 -translate-x-1/2 left-1/2 w-full px-6 py-4 flex gap-3 items-end justify-between">
-
+<div class="fixed bottom-4 -translate-x-1/2 left-1/2 w-full px-8 py-4 flex gap-3 items-end justify-between">
+    <Motion
+      tag="div"
+      class="absolute bottom-5 w-14 h-12 from-black/7 to-black/12 rounded-full origin-center group-active:scale-95 z-100"
+      :class="activeGroup ? 'bg-transparent' : 'bg-linear-to-b'"
+      :animate="{ left: `${indicatorPos}px`, }"
+      :transition="{ type: 'spring', stiffness: 400, damping: 30 }"
+    />
     <!-- bg-linear-to-b from-black/10 via-white/30 to-black/5
     dark:from-white/20 dark:via-black/30 dark:to-white/5 -->
   <Motion
@@ -44,7 +50,7 @@ const indicatorPos = computed(() => Math.max(activeItemRef.value?.offsetLeft ?? 
     :initial="containerState" :animate="containerState"
     :transition="{ type: 'spring', stiffness: 110, damping: 17, mass: 1 }"
     class="absolute
-    bg-white/80 dark:bg-[oklch(25%_0_0/0.3)]
+    bg-white/80 dark:bg-[oklch(25%_0_0/0.7)]
     backdrop-blur-md
     w-64 h-14 rounded-full shadow-lg shadow-black/10"
   >
@@ -52,21 +58,14 @@ const indicatorPos = computed(() => Math.max(activeItemRef.value?.offsetLeft ?? 
   </Motion>
    <Motion
     as="div"
-    class="absolute right-6 bg-white dark:bg-[oklch(25%_0_0/0.3)] backdrop-blur-md w-14 h-14 rounded-full shadow-lg shadow-black/10"
+    class="absolute right-8 bg-white dark:bg-[oklch(25%_0_0/0.7)] backdrop-blur-md w-14 h-14 rounded-full shadow-lg shadow-black/10"
   />
-  <div class="flex items-center justify-evenly w-60 h-14">
+  <div class="relative left-1 flex items-center justify-evenly w-56 h-14">
     <ActionBarItem v-for="item, index in items" :item :index />
   </div>
    <div class="flex items-center justify-center size-14 text-xl text-black dark:text-white font-medium transition-all duration-200 active:scale-90">
     <UIcon name="lucide:search" />
   </div>
-  <Motion
-    tag="div"
-    class="absolute bottom-5 w-14 h-12 from-black/7 to-black/12 rounded-full origin-center group-active:scale-95"
-    :class="activeGroup ? 'bg-transparent' : 'bg-linear-to-b'"
-    :animate="{ left: `${indicatorPos}px`, }"
-    :transition="{ type: 'spring', stiffness: 400, damping: 30 }"
-  />
 </div>
 
 </template>
--- app/composables/useActionBar.ts
+++ app/composables/useActionBar.ts
@@ -1,5 +1,5 @@
 const HOVER_DELAY = 400
-const DEFAULT_WIDTH = 240
+const DEFAULT_WIDTH = 228
 const DEFAULT_HEIGHT = 56
 const BORDER_RADIUS = 999
 
@@ -26,7 +26,7 @@ export const useActionBar = createSharedComposable(() => {
   const containerState = computed(() => {
     if (!activeGroup.value) return { width: width.value, height: DEFAULT_HEIGHT, borderRadius: BORDER_RADIUS, zIndex: 1 }
     const rect = activeGroup.value.getBoundingClientRect()
-    return { width: 256, height: rect.height + 8, borderRadius: 24, zIndex: 20 }
+    return { width: width.value, height: rect.height + 8, borderRadius: 24, zIndex: 20 }
   })
   const subContainerState = computed(() => objectMap(containerState.value, (k,v) => [k, v-2]))
 
