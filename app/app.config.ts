aa8b37710d6546179d3db61190769323a731887d
jimmy-lew <jimmylew9779@gmail.com>
2026-03-20 16:31:37 +0800

    feat: update base drawer style

--- app/app.config.ts
+++ app/app.config.ts
@@ -7,8 +7,18 @@ export default defineAppConfig({
     },
     drawer: {
       slots: {
+        handle: '!mt-1',
+        overlay: 'bg-black/5 dark:bg-transparent',
         content: '!rounded-t-3xl bg-[#FAFAFA] dark:bg-[#171717] ring-0'
-      }
+      },
+      compoundVariants: [
+        {
+          direction: ['bottom'],
+          class: {
+            handle: '!h-1 !w-8'
+          }
+        }
+      ]
     }
   }
 })
--- app/components/SettingsDrawer.vue
+++ app/components/SettingsDrawer.vue
@@ -8,26 +8,36 @@ const isDark = computed({
 </script>
 
 <template>
-  <UDrawer :overlay="false">
+  <UDrawer>
     <template #content>
-      <div class="flex flex-col h-screen gap-2 p-6">
-        <h1 class="font-bold text-center">Settings</h1>
+      <div class="flex flex-col h-screen gap-2 py-4 px-6">
+        <div class="grid grid-cols-3 items-center">
+          <div class="col-span-1"></div>
+          <h1 class="col-span-1 font-bold text-center">Settings</h1>
+          <h1 class="col-span-1 flex items-center justify-end">
+            <button
+              class="size-8 bg-white/5 rounded-full flex items-center justify-center duration-75 transition-all active:scale-90"
+            >
+              <UIcon name="lucide:x" />
+            </button>
+          </h1>
+        </div>
         <div class="flex flex-col gap-px mt-2">
-          <div class="flex items-center justify-between bg-black/5 dark:bg-white/5 hover:bg-black/10 dark:hover:bg-white/10 active:scale-95 transition-all duration-300 p-3 first:rounded-t-3xl last:rounded-b-3xl">
+          <div class="flex items-center justify-between bg-black/5 dark:bg-white/5 hover:bg-black/10 dark:hover:bg-white/10 active:scale-95 transition-all duration-300 px-4 py-3 first:rounded-t-3xl last:rounded-b-3xl">
             Workspace
             <span class="flex items-center gap-2">
               WIP
               <UIcon name="lucide:chevrons-up-down" />
             </span>
           </div>
-          <div class="flex items-center justify-between bg-black/5 dark:bg-white/5 hover:bg-black/10 dark:hover:bg-white/10 active:scale-95 transition-all duration-300 p-3 first:rounded-t-3xl last:rounded-b-3xl">
+          <div class="flex items-center justify-between bg-black/5 dark:bg-white/5 hover:bg-black/10 dark:hover:bg-white/10 active:scale-95 transition-all duration-300 px-4 py-3 first:rounded-t-3xl last:rounded-b-3xl">
             Notifications
             <span class="flex items-center gap-2">
               Disabled
               <UIcon name="lucide:move-up-right" />
             </span>
           </div>
-          <button @click="isDark = !isDark" class="flex items-center justify-between bg-black/5 dark:bg-white/5 hover:bg-black/10 dark:hover:bg-white/10 active:scale-95 transition-all duration-300 p-3 first:rounded-t-3xl last:rounded-b-3xl">
+          <button @click="isDark = !isDark" class="flex items-center justify-between bg-black/5 dark:bg-white/5 hover:bg-black/10 dark:hover:bg-white/10 active:scale-95 transition-all duration-300 px-4 py-3 first:rounded-t-3xl last:rounded-b-3xl">
             Appearance
             <span class="flex items-center gap-2 capitalize">
               {{ colorMode.preference }}
@@ -36,8 +46,14 @@ const isDark = computed({
           </button>
         </div>
         <div class="flex flex-col gap-px mt-2">
-          <div class="flex items-center justify-between bg-black/5 dark:bg-white/5 hover:bg-black/10 dark:hover:bg-white/10 active:scale-95 transition-all duration-300 p-3 first:rounded-t-3xl last:rounded-b-3xl">
+          <div class="flex items-center justify-between bg-black/5 dark:bg-white/5 hover:bg-black/10 dark:hover:bg-white/10 active:scale-95 transition-all duration-300 px-4 py-3 first:rounded-t-3xl last:rounded-b-3xl">
             Export data
+            <span class="flex items-center gap-2">
+              <UIcon name="lucide:move-up-right" />
+            </span>
+          </div>
+          <div class="flex items-center justify-between bg-black/5 dark:bg-white/5 hover:bg-black/10 dark:hover:bg-white/10 active:scale-95 transition-all duration-300 px-4 py-3 first:rounded-t-3xl last:rounded-b-3xl">
+            License
             <span class="flex items-center gap-2">
               <UIcon name="lucide:move-up-right" />
             </span>
