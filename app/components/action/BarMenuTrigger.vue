3fac8d6adc62831f1227876fe3c41a6327fe75c7
jimmy-lew <jimmylew9779@gmail.com>
2026-03-20 14:48:38 +0800

    fix: incorrect activation logic for menu

--- app/components/action/BarMenuTrigger.vue
+++ app/components/action/BarMenuTrigger.vue
@@ -2,14 +2,14 @@
 const { toggleMenu, menuActive } = useActionBar()
 const el = ref()
 
-onClickOutside(el, toggleMenu)
-defineShortcuts({ escape: toggleMenu })
+onClickOutside(el, () => { menuActive.value = false })
+defineShortcuts({ escape: () => { menuActive.value = false } })
 </script>
 
 <template>
 	<button
 	  ref="el"
-    @click="() => toggleMenu()"
+		@click="toggleMenu"
     class="relative flex items-center justify-center w-14 h-12 text-xl font-medium transition-all duration-200 active:scale-90 rounded-full z-10"
     :class="[ menuActive ? 'text-transparent' : 'text-black dark:text-white' ]"
   >
