0c87128d1f702a3b974b1664b4fca6fb2e25ef1d
jimmy-lew <jimmylew9779@gmail.com>
2026-03-20 14:30:30 +0800

    refactor: move bg color to layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,7 +3,7 @@
 
 <template>
 <div class="relative">
-  <div class="min-h-screen flex flex-col bg-[#edf0ea] dark:bg-[oklch(35%_0_0)]">
+  <div class="min-h-screen flex flex-col bg-[oklch(96%_0_0)] dark:bg-[oklch(0.1591_0_0)] ">
     <slot />
   </div>
    <HomeBar />
