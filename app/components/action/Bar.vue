bc2c100e6852c9bdf3140acc39f45586298ca8a4
jimmy-lew <jimmylew9779@gmail.com>
2026-03-20 21:08:49 +0800

    feat: basic search bar ui

--- app/components/action/Bar.vue
+++ app/components/action/Bar.vue
@@ -3,20 +3,32 @@ import type { NavItem } from '~/types';
 
 const props = defineProps<{ items: NavItem[] }>()
 
-const { menuState, menuActive } = useActionBar()
+const { menuState, menuActive, searchMenuState, searchActive } = useActionBar()
+const router = useRouter()
 const INDICATOR_PADDING = 4
 const activeIndex = ref(0)
 const indicatorPos = computed(() => (activeIndex.value * 56) + INDICATOR_PADDING)
+
+const handleSearch = () => {
+  router.push('/search')
+  searchActive.value = true
+}
 </script>
 
 <template>
-<div class="fixed bottom-4 -translate-x-1/2 left-1/2 w-full px-8 py-4 flex gap-3 items-end justify-center">
+<div
+  class="fixed bottom-4 -translate-x-1/2 left-1/2 w-full py-4 flex gap-3 items-end justify-center"
+  :class="[
+    searchActive ? 'px-6' : 'px-8'
+  ]"
+>
   <Motion
+    v-if="!searchActive"
     as="div"
     :initial="menuState" :animate="menuState"
     :transition="{ type: 'spring', stiffness: 110, damping: 17, mass: 1 }"
     class="
-    relative
+    relative overflow-x-hidden
     bg-action backdrop-blur-md
     border border-action-border
     rounded-full
@@ -33,32 +45,47 @@ const indicatorPos = computed(() => (activeIndex.value * 56) + INDICATOR_PADDING
       v-if="!menuActive"
       as="div"
       class="absolute bottom-0.75 w-14 h-12 bg-linear-to-b from-black/7 to-black/12 dark:from-[#2f2f2f] dark:to-[#2f2f2f] rounded-full origin-center group-active:scale-95 z-10"
-      :initial="{ left: `${indicatorPos}px`, opacity: 0 }"
-      :animate="{ left: `${indicatorPos}px`, opacity: 1}"
+      :initial="{ left: indicatorPos, opacity: 0 }"
+      :animate="{ left: indicatorPos, opacity: 1}"
       :exit="{ opacity: 0, transition: { duration: 0.05 } }"
       :transition="{ left: {type: 'spring', stiffness: 110, damping: 17, mass: 1, duration: 0.1}, opacity: { duration: 1, ease: 'linear' } }"
     />
   </Motion>
 
   <Motion
-    as="div"
+    as="button"
+    :initial="searchMenuState" :animate="searchMenuState"
     :transition="{ type: 'spring', stiffness: 110, damping: 17, mass: 1 }"
-    class="relative
+    class="relative flex
     bg-action
     backdrop-blur-md
     border border-action-border
-    w-14 h-14 rounded-full
+    rounded-full
     shadow-lg shadow-black/10"
+    @click="handleSearch"
   >
     <div
       class="flex items-center justify-center
-      size-14
+      w-14 h-full
       text-xl text-black dark:text-white font-medium
       transition-all duration-200 active:scale-90"
     >
       <UIcon name="lucide:search" />
     </div>
+    <UInput v-if="searchActive" placeholder="Search..." variant="none" :ui="{ base: 'pl-0 pr-3' }" class="-ml-2"/>
   </Motion>
+
+  <button
+    v-if="searchActive"
+    class="flex items-center justify-center
+    size-12 bg-action backdrop-blur-md
+    border border-action-border rounded-full
+    text-xl text-black dark:text-white font-medium
+    transition-all duration-200 active:scale-90"
+    @click="() => {searchActive = false; router.push('/') }"
+  >
+    <UIcon name="lucide:x" />
+  </button>
 </div>
 </template>
 
--- app/composables/useActionBar.ts
+++ app/composables/useActionBar.ts
@@ -1,5 +1,5 @@
-const DEFAULT_WIDTH = 228
-const DEFAULT_HEIGHT = 56
+const DEFAULT_WIDTH = 228 // 224px content spacing + 4px padding
+const DEFAULT_HEIGHT = 56 // 48px content height + 4px padding
 const BORDER_RADIUS = 999
 
 function objectMap<T extends object, K extends keyof T>(obj: T, f: (k: K, v: T[K]) => [K, T[K]]): T {
@@ -10,14 +10,21 @@ function objectMap<T extends object, K extends keyof T>(obj: T, f: (k: K, v: T[K
 
 export const useActionBar = createSharedComposable(() => {
   const menuActive = ref(false)
+  const searchActive = ref(false)
   const menuRef = ref<HTMLElement>()
 
   const width = ref(DEFAULT_WIDTH)
 
   const menuState = computed(() => {
-    if (!menuActive.value || !menuRef.value) return { width: width.value, height: DEFAULT_HEIGHT, borderRadius: BORDER_RADIUS }
+    if (searchActive.value) return { width: 0, borderWidth: 0 }
+    if (!menuActive.value || !menuRef.value) return { width: width.value, height: DEFAULT_HEIGHT, borderRadius: BORDER_RADIUS, borderWidth: 1 }
     const rect = menuRef.value.getBoundingClientRect()
-    return { width: width.value, height: rect.height + 8, borderRadius: 24 }
+    return { width: width.value, height: rect.height + 8, borderRadius: 24, borderWidth: 1 }
+  })
+
+  const searchMenuState = computed(() => {
+    if (!searchActive.value) return { width: 56, height: 56 }
+    return { width: 240, height: 48 }
   })
 
   const setMenuRef = (el: any) => { if (el) menuRef.value = el.$el || el }
@@ -25,5 +32,6 @@ export const useActionBar = createSharedComposable(() => {
 
   return {
     menuActive, menuState, toggleMenu, setMenuRef,
+    searchActive, searchMenuState,
   }
 })
