ebd2e654294287f727c0ebc45cac5af79c5e5de3
jimmy-lew <jimmylew9779@gmail.com>
2026-03-20 21:22:52 +0800

    feat: adjust padding and layout

--- app/pages/search.vue
+++ app/pages/search.vue
@@ -4,7 +4,7 @@
 
 <template>
 	<div class="relative flex flex-col items-center font-mono px-8 py-4 gap-6">
-    <div class="flex items-center justify-between w-full">
+    <div class="flex items-center justify-between w-full pt-8">
       <h1 class="text-xl font-black">Search</h1>
       <div class="flex items-center justify-center rounded-full bg-black/5 dark:bg-white/5 size-8">
         <UIcon name="lucide:edit" />
