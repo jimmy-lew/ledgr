2c5c52b7019242b422b5f6def32f943ccef8ac51
jimmy-lew <jimmylew9779@gmail.com>
2026-03-20 21:28:13 +0800

    refactor: remove dead code

--- app/components/home/Bar.vue
+++ app/components/home/Bar.vue
@@ -1,17 +1,4 @@
 <script setup lang="ts">
-import { CustomiseDrawer, StatementUpload, WidgetCreate } from '#components';
-
-const overlay = useOverlay()
-const router = useRouter()
-
-const statementUploadModal = overlay.create(StatementUpload)
-const createWidgetModal = overlay.create(WidgetCreate)
-const customiseDrawer = overlay.create(CustomiseDrawer)
-
-const handleAddStatement = () => {
-  statementUploadModal.open()
-}
-
 const items = [
   { icon: 'lucide:home', to: '/' },
   { icon: 'lucide:chart-pie', to: '/budget' },
@@ -20,7 +7,5 @@ const items = [
 </script>
 
 <template>
-  <!-- <NavBar :items/> -->
-
   <ActionBar :items class="rounded-3xl"/>
 </template>
