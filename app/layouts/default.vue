bc01fe34a139dbe8152f16e42c711dec992ef071
jimmy-lew <jimmylew9779@gmail.com>
2026-03-22 15:37:03 +0800

    refactor: shift col layout to layout component

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,12 +2,10 @@
 </script>
 
 <template>
-<div class="relative">
-  <div class="min-h-screen flex flex-col bg-[oklch(96%_0_0)] dark:bg-[#070707] ">
+  <div class="relative min-h-screen flex flex-col bg-[oklch(96%_0_0)] dark:bg-[#070707] font-mono px-8 pt-4 pb-24 gap-6">
     <slot />
   </div>
-   <HomeBar />
-</div>
+  <HomeBar />
 </template>
 
 <style scoped>
--- app/pages/search.vue
+++ app/pages/search.vue
@@ -8,20 +8,18 @@ onMounted(() => {
 </script>
 
 <template>
-	<div class="relative flex flex-col items-center min-h-screen font-mono px-8 pt-4 pb-24 gap-6">
-    <div class="flex items-center justify-between w-full pt-6">
-      <h1 class="text-2xl font-bold">Search</h1>
-      <div class="flex items-center justify-center rounded-full bg-black/5 dark:bg-white/5 size-12 text-lg">
-        <UIcon name="lucide:edit" />
-      </div>
-    </div>
-    <div class="flex flex-col w-full gap-1">
-      <span class="font-medium text-muted text-sm mb-2">Recently viewed</span>
-      <div class="w-full bg-black/5 dark:bg-white/5 h-12 rounded-md"></div>
-      <div class="w-full bg-black/5 dark:bg-white/5 h-12 rounded-md"></div>
-      <div class="w-full bg-black/5 dark:bg-white/5 h-12 rounded-md"></div>
-      <div class="w-full bg-black/5 dark:bg-white/5 h-12 rounded-md"></div>
-      <div class="w-full bg-black/5 dark:bg-white/5 h-12 rounded-md"></div>
-    </div>
-	</div>
+<div class="flex items-center justify-between w-full pt-6">
+  <h1 class="text-2xl font-bold">Search</h1>
+  <div class="flex items-center justify-center rounded-full bg-black/5 dark:bg-white/5 size-12 text-lg">
+    <UIcon name="lucide:edit" />
+  </div>
+</div>
+<div class="flex flex-col w-full gap-1">
+  <span class="font-medium text-muted text-sm mb-2">Recently viewed</span>
+  <div class="w-full bg-black/5 dark:bg-white/5 h-12 rounded-md"></div>
+  <div class="w-full bg-black/5 dark:bg-white/5 h-12 rounded-md"></div>
+  <div class="w-full bg-black/5 dark:bg-white/5 h-12 rounded-md"></div>
+  <div class="w-full bg-black/5 dark:bg-white/5 h-12 rounded-md"></div>
+  <div class="w-full bg-black/5 dark:bg-white/5 h-12 rounded-md"></div>
+</div>
 </template>
