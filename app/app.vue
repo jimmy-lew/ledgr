4fc282dd7337c83d3f33e080a8c26762a695b4a4
jimmy <jimmylew9779@gmail.com>
2026-03-26 21:55:02 +0800

    feat: fix keyboard jump on mobile

--- app/app.vue
+++ app/app.vue
@@ -5,7 +5,7 @@ const description = ''
 useHead({
   meta: [
     { charset: 'utf-8' },
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1, interactive-widget=overlays-content' },
   ],
   link: [{ rel: 'icon', href: '/favicon.png' }],
   htmlAttrs: {
