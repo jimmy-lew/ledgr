3a50580015d1a79830ae8c84d0c1e7bc9bd830d3
jimmy-lew <jimmylew9779@gmail.com>
2026-03-28 22:52:17 +0800

    refactor: remove dead html

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -18,34 +18,9 @@ const { searchActive } = useActionBar()
         <h1 class="text-3xl font-bold">$20,000.00</h1>
     </div>
 </div>
-<!-- <UTextarea
-  variant="none"
-  placeholder="Add your latest transaction here... [Item <amount>]"
-  :max-rows="3"
-  autoresize
-  :ui="{ base: 'text-2xl px-0' } "
-/> -->
 <HomeGlanceInsights />
 <HomeWidget />
 <HomeGlanceTransactions />
-
-<!-- <div class="relative flex flex-col items-center min-h-screen w-full font-mono overflow-hidden pb-24">
-  Test -->
-  <!-- <div class="absolute rounded-xl bg-cover bg-no-repeat bg-[url('https://i.pinimg.com/736x/d6/ac/44/d6ac44091afa772b3744f1ecad3d33e3.jpg')] w-72 h-42 right-12 top-12 sm:left-12"></div>
-  <div class="absolute flex flex-col gap-4 p-8 rounded-xl bg-white/10 backdrop-blur-sm sm:backdrop-blur-xs shadow-md w-72 h-42 left-12 top-18 sm:top-12">
-    <h2 class="text-white font-bold">Balance</h2>
-    <div class="flex items-start justify-start gap-1">
-      <h1 class="text-3xl font-black text-white">19128.33</h1>
-      <span class="text-xs text-green-400 filter drop-shadow-lg drop-shadow-green-500">
-        <UIcon name="lucide:chevron-up"/>
-        67%
-      </span>
-    </div>
-  </div>
-  <HomeGlanceInsights />
-  <HomeWidget />
-  <HomeGlanceTransactions /> -->
-<!-- </div> -->
 </template>
 
 <style scoped>
