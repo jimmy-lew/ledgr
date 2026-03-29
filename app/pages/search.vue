3d2235685e971f8a4e33c84f97e870b36721fc1b
jimmy-lew <jimmylew9779@gmail.com>
2026-03-29 15:07:20 +0800

    feat: sample data

--- app/pages/search.vue
+++ app/pages/search.vue
@@ -4,17 +4,43 @@
 <template>
 <div class="flex items-center justify-between w-full pt-6">
   <h1 class="text-2xl font-bold">Search</h1>
-
   <div class="flex items-center justify-center gap-4 rounded-full bg-black/5 dark:bg-white/5 h-12 px-3.75 py-2 text-lg">
     <UIcon name="lucide:edit" />
   </div>
 </div>
 <div class="flex flex-col w-full gap-1">
-  <span class="font-medium text-muted text-sm mb-2">Recently viewed</span>
-  <div class="w-full bg-black/5 dark:bg-white/5 h-12 rounded-md"></div>
-  <div class="w-full bg-black/5 dark:bg-white/5 h-12 rounded-md"></div>
-  <div class="w-full bg-black/5 dark:bg-white/5 h-12 rounded-md"></div>
-  <div class="w-full bg-black/5 dark:bg-white/5 h-12 rounded-md"></div>
-  <div class="w-full bg-black/5 dark:bg-white/5 h-12 rounded-md"></div>
+  <span class="font-medium text-muted text-sm mb-3">Recently viewed</span>
+  <div class="w-full flex items-center gap-2 py-2">
+    <div class="flex items-center justify-center size-4 rounded-full bg-[#555fbc]">
+      <UIcon name="lucide:check" class="text-[#070707] text-xs font-medium" />
+    </div>
+    <span class="font-medium text-sm">
+      Load Game Feature
+    </span>
+  </div>
+  <div class="w-full flex items-center gap-2 py-2">
+    <div class="flex items-center justify-center size-4 rounded-full bg-[#555fbc]">
+      <UIcon name="lucide:check" class="text-[#070707] text-xs font-medium" />
+    </div>
+    <span class="font-medium text-sm">
+      Load Game Feature
+    </span>
+  </div>
+  <div class="w-full flex items-center gap-2 py-2">
+    <div class="flex items-center justify-center size-4 rounded-full bg-[#555fbc]">
+      <UIcon name="lucide:check" class="text-[#070707] text-xs font-medium" />
+    </div>
+    <span class="font-medium text-sm">
+      Load Game Feature
+    </span>
+  </div>
+  <div class="w-full flex items-center gap-2 py-2">
+    <div class="flex items-center justify-center size-4 rounded-full bg-[#555fbc]">
+      <UIcon name="lucide:check" class="text-[#070707] text-xs font-medium" />
+    </div>
+    <span class="font-medium text-sm">
+      Load Game Feature
+    </span>
+  </div>
 </div>
 </template>
