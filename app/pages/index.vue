86ab843810f65bda473158e36d4288d6e3d1558c
jimmy-lew <jimmylew9779@gmail.com>
2026-03-29 15:39:24 +0800

    feat: move title and buttons to layout

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,13 +5,6 @@ const { searchActive } = useActionBar()
 
 <template>
 
-<div class="flex items-center justify-between w-full pt-6">
-  <h1 class="text-2xl font-bold">Home</h1>
-  <div class="flex items-center justify-center gap-4 rounded-full bg-black/5 dark:bg-white/5 h-12 px-3.75 py-2 text-lg">
-    <UIcon name="lucide:edit" />
-    <UIcon name="lucide:ellipsis" />
-  </div>
-</div>
 <div class="flex flex-col items-center rounded-2xl bg-white dark:bg-[oklch(0.1448_0_0)] shadow-base-sm px-6 py-3 aspect-video">
     <div class="flex flex-col items-center justify-center w-full h-full">
         <span class="text-muted">Balance</span>
--- app/pages/search.vue
+++ app/pages/search.vue
@@ -1,13 +1,21 @@
 <script setup lang="ts">
+definePageMeta({
+  layout: {
+    props: {
+      title: 'Search',
+      icons: ['lucide:edit']
+    },
+  },
+})
 </script>
 
 <template>
-<div class="flex items-center justify-between w-full pt-6">
+<!-- <div class="flex items-center justify-between w-full pt-6">
   <h1 class="text-2xl font-bold">Search</h1>
   <div class="flex items-center justify-center gap-4 rounded-full bg-black/5 dark:bg-white/5 h-12 px-3.75 py-2 text-lg">
-    <UIcon name="lucide:edit" />
+    <UIcon name="lucide:edit" class="transition-all duration-75 active:scale-90"/>
   </div>
-</div>
+</div> -->
 <div class="flex flex-col w-full gap-1">
   <span class="font-medium text-muted text-sm mb-3">Recently viewed</span>
   <div class="w-full flex items-center gap-2 py-2">
