f7aee1837efcbee5c9f48dd2b9edcb6047bc4528
jimmy-lew <jimmylew9779@gmail.com>
2026-04-03 13:04:46 +0800

    feat: reduce gap again

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -9,7 +9,7 @@ withDefaults(defineProps<{
 </script>
 
 <template>
-  <div class="relative min-h-screen flex flex-col bg-[oklch(96%_0_0)] dark:bg-[#070707] font-mono pt-4 gap-3">
+  <div class="relative min-h-screen flex flex-col bg-[oklch(96%_0_0)] dark:bg-[#070707] font-mono pt-4 gap-2">
     <div class="flex items-center justify-between w-full pt-6 px-5">
       <h1 class="text-2xl font-bold">{{ title }} </h1>
       <div class="flex items-center justify-center gap-4 rounded-full bg-black/5 dark:bg-white/5 h-12 px-3.75 py-2 text-lg">
