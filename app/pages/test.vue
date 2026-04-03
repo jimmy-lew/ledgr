da836ccabf6849525c3373f8121b0530790ae122
jimmy-lew <jimmylew9779@gmail.com>
2026-04-03 10:29:21 +0800

    feat: update test page

--- app/pages/test.vue
+++ app/pages/test.vue
@@ -1,6 +1,12 @@
-<script setup>
+<script setup lang="ts">
 </script>
 
 <template>
-<div></div>
+<div>
+  <div class="flex items-center justify-center gap-4 w-full">
+    <UIcon name="lucide:home" class="stroke-1"/>
+    <UIcon name="lucide:home" class="stroke-2"/>
+    <UIcon name="lucide:home" class="stroke-3"/>
+  </div>
+</div>
 </template>
