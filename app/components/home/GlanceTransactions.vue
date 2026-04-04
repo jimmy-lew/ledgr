3b3647a540fddbda50bdd9a11697b8f3c6235741
jimmy-lew <jimmylew9779@gmail.com>
2026-04-04 13:54:02 +0800

    refactor: more readable titles

--- app/components/home/GlanceTransactions.vue
+++ app/components/home/GlanceTransactions.vue
@@ -3,22 +3,22 @@
 <template>
 <div class="w-full flex flex-col gap-2">
   <div class="flex flex-col gap-0.5">
-    <TransactionItem id="1" date="30/11/2025" type="ADVICE_FAST_PAYMENT_RECEIPT" description="" :withdrawal="null" :deposit="13" />
-    <TransactionItem id="2" date="30/11/2025" type="ADVICE_FAST_PAYMENT_RECEIPT" description="" :withdrawal="null" :deposit="13" />
-    <TransactionItem id="3" date="01/12/2025" type="DEBIT_CARD_TRANSACTION" description="" :withdrawal="8.6" :deposit="null" />
-    <TransactionItem id="4" date="30/11/2025" type="ADVICE_FAST_PAYMENT_RECEIPT" description="" :withdrawal="null" :deposit="13" />
-    <TransactionItem id="5" date="30/11/2025" type="ADVICE_FAST_PAYMENT_RECEIPT" description="" :withdrawal="null" :deposit="13" />
-    <TransactionItem id="6" date="01/12/2025" type="DEBIT_CARD_TRANSACTION" description="" :withdrawal="8.6" :deposit="null" />
-    <TransactionItem id="7" date="30/11/2025" type="ADVICE_FAST_PAYMENT_RECEIPT" description="" :withdrawal="null" :deposit="13" />
-    <TransactionItem id="8" date="30/11/2025" type="ADVICE_FAST_PAYMENT_RECEIPT" description="" :withdrawal="null" :deposit="13" />
-    <TransactionItem id="9" date="30/11/2025" type="ADVICE_FAST_PAYMENT_RECEIPT" description="" :withdrawal="null" :deposit="13" />
-    <TransactionItem id="10" date="01/12/2025" type="DEBIT_CARD_TRANSACTION" description="" :withdrawal="8.6" :deposit="null" />
-    <TransactionItem id="11" date="30/11/2025" type="ADVICE_FAST_PAYMENT_RECEIPT" description="" :withdrawal="null" :deposit="13" />
-    <TransactionItem id="12" date="01/12/2025" type="DEBIT_CARD_TRANSACTION" description="" :withdrawal="8.6" :deposit="null" />
-    <TransactionItem id="13" date="01/12/2025" type="DEBIT_CARD_TRANSACTION" description="" :withdrawal="8.6" :deposit="null" />
-    <TransactionItem id="14" date="30/11/2025" type="ADVICE_FAST_PAYMENT_RECEIPT" description="" :withdrawal="null" :deposit="13" />
-    <TransactionItem id="15" date="30/11/2025" type="ADVICE_FAST_PAYMENT_RECEIPT" description="" :withdrawal="null" :deposit="13" />
-    <TransactionItem id="16" date="01/12/2025" type="DEBIT_CARD_TRANSACTION" description="" :withdrawal="8.6" :deposit="null" />
+    <TransactionItem id="1" date="30/11/2025" type="Fast Payment Receipt" description="" :withdrawal="null" :deposit="13" />
+    <TransactionItem id="2" date="30/11/2025" type="Fast Payment Receipt" description="" :withdrawal="null" :deposit="13" />
+    <TransactionItem id="3" date="01/12/2025" type="Debit Card Transaction" description="" :withdrawal="8.6" :deposit="null" />
+    <TransactionItem id="4" date="30/11/2025" type="Fast Payment Receipt" description="" :withdrawal="null" :deposit="13" />
+    <TransactionItem id="5" date="30/11/2025" type="Fast Payment Receipt" description="" :withdrawal="null" :deposit="13" />
+    <TransactionItem id="6" date="01/12/2025" type="Debit Card Transaction" description="" :withdrawal="8.6" :deposit="null" />
+    <TransactionItem id="7" date="30/11/2025" type="Fast Payment Receipt" description="" :withdrawal="null" :deposit="13" />
+    <TransactionItem id="8" date="30/11/2025" type="Fast Payment Receipt" description="" :withdrawal="null" :deposit="13" />
+    <TransactionItem id="9" date="30/11/2025" type="Fast Payment Receipt" description="" :withdrawal="null" :deposit="13" />
+    <TransactionItem id="10" date="01/12/2025" type="Debit Card Transaction" description="" :withdrawal="8.6" :deposit="null" />
+    <TransactionItem id="11" date="30/11/2025" type="Fast Payment Receipt" description="" :withdrawal="null" :deposit="13" />
+    <TransactionItem id="12" date="01/12/2025" type="Debit Card Transaction" description="" :withdrawal="8.6" :deposit="null" />
+    <TransactionItem id="13" date="01/12/2025" type="Debit Card Transaction" description="" :withdrawal="8.6" :deposit="null" />
+    <TransactionItem id="14" date="30/11/2025" type="Fast Payment Receipt" description="" :withdrawal="null" :deposit="13" />
+    <TransactionItem id="15" date="30/11/2025" type="Fast Payment Receipt" description="" :withdrawal="null" :deposit="13" />
+    <TransactionItem id="16" date="01/12/2025" type="Debit Card Transaction" description="" :withdrawal="8.6" :deposit="null" />
   </div>
 </div>
 </template>
