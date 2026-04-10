294f8de1e7b8df4360262984d5395c79410714ad
jimmy-lew <jimmylew9779@gmail.com>
2026-04-10 22:24:38 +0800

    feat: pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@vueuse/nuxt', '@nuxt/hints', '@nuxt/fonts', '@nuxt/ui', 'motion-v/nuxt'],
+  modules: [
+    '@nuxt/eslint',
+    '@vueuse/nuxt',
+    '@nuxt/hints',
+    '@nuxt/fonts',
+    '@nuxt/ui',
+    'motion-v/nuxt',
+    '@vite-pwa/nuxt'
+  ],
   ssr: false,
   css: ['~/assets/css/tailwind.css'],
   imports: { dirs: ['types/**'] },
@@ -8,6 +16,28 @@ export default defineNuxtConfig({
   icon: { mode: 'svg' },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'ledgr',
+      short_name: 'ledgr',
+      description: 'ledgr pwa app',
+      display: 'standalone',
+      icons: [
+        {
+          src: '/pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: '/pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        },
+      ],
+    }
+  },
   app: {
     pageTransition: { name: 'page', mode: 'out-in' }
   }
