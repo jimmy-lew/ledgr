59789e9444e790618438780eb375fd39ba4f4e44
jimmy-lew <jimmylew9779@gmail.com>
2026-04-10 19:32:36 +0800

    feat: collapse animation for sidebar

--- app/components/HomeSidebar.vue
+++ app/components/HomeSidebar.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
 
+const isCollapsed = ref(false)
+
+defineShortcuts({
+  '[': () => { isCollapsed.value = !isCollapsed.value }
+})
+
+const sidebarUi = computed(() => ({
+  root: [
+    'border-0 min-w-2 transition-[width_400ms,top_250ms] ease-out',
+    isCollapsed.value ? 'top-16 -left-16' : 'top-0 left-0'
+  ]
+}))
 </script>
 
 <template>
-  <UDashboardSidebar collapsible resizable :ui="{ root: 'border-0' }">
-      Test
+  <UDashboardSidebar
+    v-model:collapsed="isCollapsed"
+    collapsible
+    resizable
+    :ui="sidebarUi"
+  >
+    Test
   </UDashboardSidebar>
 </template>
